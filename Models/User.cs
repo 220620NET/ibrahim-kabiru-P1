a92a112741650b99710d66dcc4cbc04aa328786f
kabiru <ibrahimkabiru29@gmail.com>
2022-06-25 14:13:16 -0400

    This is user Model calss

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace model;
+
+public class user {
+
+private string userID{get; set;}
+
+private string userName{get; set;}
+private string Password{get; set;}
+private string role{get; set;}
+}
+
+
+
+
+
