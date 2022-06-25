c2e7837d0a6da4c3321df523578b94d637030844
kabiru <ibrahimkabiru29@gmail.com>
2022-06-25 14:27:04 -0400

    This is constructor of the class model

--- Models/User.cs
+++ Models/User.cs
@@ -7,8 +7,18 @@ private string userID{get; set;}
 private string userName{get; set;}
 private string Password{get; set;}
 private string role{get; set;}
+
+
+public void user(string userID, string userName, string Password, string role){
+    this.userID = userID;
+    this.userName = userName;
+    this.Password = Password;
+    this.role = role;
 }
 
+}
+
+ 
 
 
 
